f92ccaaa932936d321ac69c529eadfd35e8f9ef7
taso131 <taso131@hotmail.com>
2021-09-06 19:21:37 +0200

    Update main.py
    updated prints

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datetime import date
 
+string = ""
 # Press Umschalt+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -14,18 +15,27 @@ def print_welcome():
     hlppls = Talk to my supervisor.\n
     extendedhlppls = Get more help commands""") 
     
+#rabatt system
+def print_invoice(capsule_units_to_purchase):
+    print("|-------------------------------------------|")
+    print(f"| --- {get_total_sum()} € ---|")
+    print("|-------------------------------------------|")
 
-
+def get_total_sum():
+        #discount
 
 def handle_input():
     command = input()
     if command == "pinfo":
         print("New empty Capsules made of HPMC (Hydroxypropylmethylcellulose)")
-        if command == 'pbuy':
-            print()
-        elif command == 'hlppls':
-            print()
-        elif command == 'extendedhlppls':
+    if command == 'pbuy':
+            print("How many units of Capsules do you want to buy? 1 Unit = 1.000 Capsules")
+            capsule_units_to_purchase = input()
+            print_invoice(capsule_units_to_purchase)
+            
+    elif command == 'hlppls':
+            print("ww1")
+    elif command == 'extendedhlppls':
             print("""
             icantwait = Wanna know when your Package might arrive?\n
             idontwantitanymore = Unhappy with our Service? :( Just tell us! We'll find a solution\n
@@ -34,19 +44,19 @@ def handle_input():
             isyourstuffevengood = Worrying that those capsules are out of bad quality? Or not as described? Just see one of our reviews\n
             howdoiusethisshhh = Need explanation on how to improve your usage with our HPMC-Capsules? Say no more!\n
             """)
-        elif command == 'icantwait':
+    elif command == 'icantwait':
             print("The moment you order, you'll get it by 2-4 Days in Europe. 7-10 Days in America and 10-14 Days worldwide")
-        elif command == 'idontwantitanymore':
+    elif command == 'idontwantitanymore':
             print(
                 "You may return your purchase for no reason in 14 days and you can apply for a return within 90 days "
                 "if there is a reason")
-        elif command == 'wenexpirelul':
+    elif command == 'wenexpirelul':
             print(get_expiry_date())
-        elif command == 'canipaywithbitcoins':
+    elif command == 'canipaywithbitcoins':
             print("You can pay with BTC, ETH, ADA, ONE and RBC")
-        elif command == 'isyourstuffevengood':
+    elif command == 'isyourstuffevengood':
             print("Still not 100% if it fits for you? Look at the dozens of reviews we already have gotten since launch")
-        elif command == 'howdoiusethisshhh':
+    elif command == 'howdoiusethisshhh':
             print("For detailed instructions you can watch our video on youtube.com")
 
         #best before date = 1 year from date given the person wants to order
