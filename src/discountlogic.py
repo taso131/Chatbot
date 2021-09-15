b9ca8179a6716f876213e7a2947c559a52205bf2
taso131 <taso131@hotmail.com>
2021-09-15 14:47:11 +0200

    new discountlogic class implemented

--- src/discountlogic.py
+++ src/discountlogic.py
@@ -1,25 +1,45 @@
 import os
 
-new_customer: bool  # would come from a database
+product_cost = 19.99
+
+#Decimal numbers are percentage numbers
+promotions_dictionary = {
+    'no_discount' : 1,
+    'first_order_50_off' : 0.5,
+    'first_order_rest_25_off' : 0.75,
+    'more_than_3' : 0.9,
+    'more_than_10' : 0.8
+}
 
 def get_total_sum(customer_data) -> float:
     # the prices are for capsules with no filling and no aroma
     amount_to_buy = float(customer_data[2])
+    
     if first_time_customer(customer_data):  # need to be added.
-        print("worked")
+        return calculations_for_new_customer(amount_to_buy)
+    
+    if amount_to_buy < 3:
+        return (amount_to_buy * product_cost) * promotions_dictionary['no_discount']
+    
+    elif amount_to_buy >= 3 and amount_to_buy < 10:
+        return (amount_to_buy * product_cost) * promotions_dictionary['more_than_3']
+    
     else:
-        print("worked again, but denied")
-       # if amount_to_buy < 3:
-            # 10 euro discount / 1pcs. = 50% / 2pcs. = 25%
-       #     return(amount_to_buy * 19.99-10)
-      #  else:
-     #       return(amount_to_buy * 15.99)  # 20% discount
-    #else:
-    #    if amount_to_buy < 3:
-   #         return(amount_to_buy * 19.99)  # no discount
-  #      else:
-##            return(amount_to_buy * 17.99)  # 10% discount
-
+        return (amount_to_buy * product_cost) * promotions_dictionary['more_than_10']
+        
+        
+        
+def calculations_for_new_customer(amount_to_buy) -> float:
+    if amount_to_buy < 2:
+        return product_cost * promotions_dictionary['first_time_50_off']
+        
+    else:
+        first_one_50_off = product_cost * promotions_dictionary['first_time_50_off']
+        not_qualified_amount = amount_to_buy - 1    
+        left_ones_25_off = not_qualified_amount * promotions_dictionary['first_order_rest_25_off']
+    
+        return first_one_50_off + left_ones_25_off
+    
 
 def first_time_customer(customer_data) -> bool:
     first_name = customer_data[0]
